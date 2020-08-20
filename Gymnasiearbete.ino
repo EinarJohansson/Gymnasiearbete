3e853f95a0fd6c187cd7d46f48ecd84558a08aff
EinarJohansson <ejrobb09@gmail.com>
2020-08-20 20:57:34 +0200

    Lägger första comitten - for the vine.

---
+++ Gymnasiearbete.ino
@@ -0,0 +1,16 @@
+/*
+Einar Johansson, Albert Johansson, Ludvig Bergstrand
+TE18A
+Gymnasiearbete
+*/
+
+// setup funktionen körs en gång innan loopen:
+void setup() {
+
+}
+
+// loop funktionen kallas kontinuerligt:
+void loop()
+{
+
+}
